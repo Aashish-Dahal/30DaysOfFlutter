5b745cb4248a147266fb06f38282471208e5174a
Aashish-Dahal <ashishdahal490@gmail.com>
2021-05-14 15:54:47 +0545

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {"/": (context) => HomePage()},
     );
   }
 }
