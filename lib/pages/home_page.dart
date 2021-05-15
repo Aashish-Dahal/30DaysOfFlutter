422a27c9d052c4bd8de8cfdcbd6fd2b60062a41d
Aashish-Dahal <ashishdahal490@gmail.com>
2021-05-15 11:59:34 +0545

    day 11 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,34 @@
+import 'dart:convert';
+
 import 'package:demo_app/models/catalog.dart';
 import 'package:demo_app/widgets/itemWidget.dart';
 import 'package:demo_app/widgets/mydrawer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class HomePage extends StatelessWidget {
-  //variables in flutter
-  final int days = 30;
-  final String name = "Codeish";
-  final bool isMale = true;
-  final double pi = 3.14;
-  final num = 134;
-  final appName = "DemoApp";
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson.toString());
+    var productData = decodedData['products'];
+    CatalogModel.items =
+        List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dumyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
         appBar: AppBar(
           title: Text(
@@ -22,14 +37,16 @@ class HomePage extends StatelessWidget {
         ),
         body: Padding(
           padding: const EdgeInsets.all(10.0),
-          child: ListView.builder(
-            itemCount: dumyList.length, //CatalogModel.items.length,
-            itemBuilder: (context, index) {
-              return ItemWidget(
-                  item: dumyList[index] //CatalogModel.items[index],
-                  );
-            },
-          ),
+          child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+              ? ListView.builder(
+                  itemCount: CatalogModel.items.length,
+                  itemBuilder: (context, index) {
+                    return ItemWidget(
+                      item: CatalogModel.items[index],
+                    );
+                  },
+                )
+              : LinearProgressIndicator(),
         ),
         drawer: MyDrawer());
   }
